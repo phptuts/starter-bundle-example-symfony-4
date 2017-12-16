b831d07f08b26cd843954861261d42b5056d51ea
phptuts <noah.php@gmail.com>
2017-12-16 00:06:06 -0600

    Testing Symfony 4 Example

--- scripts/switch_to_symfony_4.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$xmlPath = __DIR__ . '/../vendor/start-kit-symfony/start-bundle/phpunit.xml.dist';
-$xmlString = file_get_contents($xmlPath);
-$xmlString = str_replace('<server name="KERNEL_CLASS" value="AppKernel" />', '<env name="KERNEL_CLASS" value="App\Kernel" />', $xmlString);
-file_put_contents($xmlPath, $xmlString);
--- tests/Controller/ForgetPasswordWorkFlowTest.php
+++ tests/Controller/ForgetPasswordWorkFlowTest.php
@@ -6,7 +6,6 @@ use App\Entity\User;
 use PHPUnit\Framework\Assert;
 use StarterKit\StartBundle\Repository\UserRepository;
 use StarterKit\StartBundle\Service\AuthResponseService;
-use StarterKit\StartBundle\Tests\Controller\RequestTrait;
 use Symfony\Component\BrowserKit\Cookie;
 use Symfony\Component\HttpFoundation\Request;
 use App\Tests\BaseTestCase;
